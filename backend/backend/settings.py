7c1a45a26f71ff8a3f12dd350d785d9d9ee23911
NijanthR <srinijan2405@gmail.com>
2026-04-07 22:55:26 +0530

    f

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 cors_origins = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173')
-CORS_ALLOW_ALL_ORIGINS = env_bool('CORS_ALLOW_ALL_ORIGINS', False)
+CORS_ALLOW_ALL_ORIGINS = env_bool('CORS_ALLOW_ALL_ORIGINS', True)
 CORS_ALLOWED_ORIGINS = [origin.strip() for origin in cors_origins.split(',') if origin.strip()]
 
 csrf_trusted = os.getenv('CSRF_TRUSTED_ORIGINS', '')
